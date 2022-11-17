445ff930dc5e442f848677897db1c45620b0b021
Mateo1310 <mrodriguez81813@umanizales.edu.co>
2022-11-17 15:05:17 -0500

    aAñadiendo Barra de navegacion y sus estilos

---
+++ src/components/nav-bar/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.css'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const Navegacion = () => {
+    return(
+        <div>
+        <Navbar bg="dark" variant="dark" className="">
+            <Navbar.Brand href="#home" className="mx-3 fs-3">Mateo Rodriguez Garcia <span>Desarrollador Web Junior</span></Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end mx-3"/>
+            <Nav className="me-auto">
+                <Nav.Link href="#quien-soy">¿Quien soy?</Nav.Link>
+                <Nav.Link href="#experiencia">Experiencia</Nav.Link>
+                <Nav.Link href="#educacion">Educación</Nav.Link>
+                <Nav.Link href="#Habilidades">Habilidades</Nav.Link>
+                <Nav.Link href="#contacto">Contacto</Nav.Link>
+            </Nav>
+        </Navbar>   
+        </div>
+    );
+};
+
+export default Navegacion;
