d29618c7c22b93c30b11498eb6d050f79e0370f9
Mateo1310 <mrodriguez81813@umanizales.edu.co>
2022-11-18 03:20:57 -0500

    ÃAñadiendo componente Experience

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navegacion from './components/nav-bar';
 import Header from './components/header';
+import Experience from './components/experience';
 
 const App = () => {
   return (
     <div className="App">
       <Navegacion id='home'/>
       <Header id='quien-soy'/>
+      <Experience id='experiencia'/>
     </div>
   );
 }
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './styles/styles.css'
+import './styles/index.css'
 import { Container, Row } from "react-bootstrap";
 import Foto from './img/foto-mateo.png';
 
