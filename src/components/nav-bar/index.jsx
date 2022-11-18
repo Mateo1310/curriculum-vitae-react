7a196a042357089f25fdef336d2b897380235157
Mateo1310 <mrodriguez81813@umanizales.edu.co>
2022-11-18 01:33:30 -0500

    Transformando responsive la Navbar

--- src/components/nav-bar/index.jsx
+++ src/components/nav-bar/index.jsx
@@ -3,22 +3,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.css'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import Container from "react-bootstrap/Container";
 
 const Navegacion = () => {
     return(
         <div>
-        <Navbar bg="dark" variant="dark" className="">
-            <Navbar.Brand href="#home" className="mx-3 fs-3">Mateo Rodriguez Garcia <span>Desarrollador Web Junior</span></Navbar.Brand>
-            <Navbar.Collapse className="justify-content-end mx-3"/>
-            <Nav className="me-auto">
-                <Nav.Link href="#quien-soy">¿Quien soy?</Nav.Link>
-                <Nav.Link href="#experiencia">Experiencia</Nav.Link>
-                <Nav.Link href="#educacion">Educación</Nav.Link>
-                <Nav.Link href="#Habilidades">Habilidades</Nav.Link>
-                <Nav.Link href="#contacto">Contacto</Nav.Link>
-            </Nav>
-        </Navbar>   
+            <Navbar bg="dark" variant="dark" expand="xl" fixed="top">
+                <Container fluid>
+                    <Navbar.Brand href="#home" className="fs-3 mx-3">Mateo Rodriguez Garcia <span>Desarrollador Web</span></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll"/>
+                    <Navbar.Collapse id="navbarScroll" className="justify-content-end">
+                        <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '250px' }} navbarScroll>
+                        |   <Nav.Link href="#quien-soy">¿Quien soy?</Nav.Link>
+                            <Nav.Link href="#experiencia">Experiencia</Nav.Link>
+                            <Nav.Link href="#educacion">Educación</Nav.Link>
+                            <Nav.Link href="#Habilidades">Habilidades</Nav.Link>
+                            <Nav.Link href="#contacto">Contacto</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>    
+            </Navbar>
         </div>
     );
 };
